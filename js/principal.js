25b0ef121b4dfe1410a9c12374f6d210609ef5c2
Irene Riportella <riportellairene@gmail.com>
2022-06-10 10:59:54 +0200

    playing withconditions

--- js/principal.js
+++ js/principal.js
@@ -4,13 +4,28 @@ let peso = tdpeso.textContent;
 let tdaltura = paciente.querySelector('.info-altura');
 let altura = tdaltura.textContent;
 let tdIMC = paciente.querySelector('.info-imc');
-let imc = peso / (altura * altura);
-tdIMC.textContent = imc;
+
+let pesoEsValido = true;
+let alturaEsValida = true;
+
+//verdadero o falso ---> verdadero
 
 if (peso < 0 || peso > 1000) {
   console.log('Peso incorrecto');
+  tdIMC.textContent = 'Peso incorrecto';
+  pesoEsValido = false;
 }
 
-if (altura < 0 || altura > 4.0) {
+//verdadero o falso ---> verdadero
+if (altura < 0 || altura > 3.0) {
   console.log('Altura incorrecta');
+  tdIMC.textContent = 'Altura incorrecta';
+  alturaEsValida = false;
+}
+
+//verdadero y ferdadero ---> verdadero
+//verdadero y falso ---> falso
+if (pesoEsValido && alturaEsValida) {
+  let imc = peso / (altura * altura);
+  tdIMC.textContent = imc;
 }
