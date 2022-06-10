7b4939cf07914c73ca53a56a9aa37aa88ed2ac92
Irene Riportella <riportellairene@gmail.com>
2022-06-10 09:33:04 +0200

    created principal.js

---
+++ js/principal.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('.titulo');
+console.log(titulo.textContent);
+console.log(titulo);
+titulo.textContent = 'Buena Vida Nutrición';
+console.log('Estoy imprimiendo de una fuente externa que es principal.js');
