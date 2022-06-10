4716b6a325bb73268a895cb3a05a10f5cf455395
Irene Riportella <riportellairene@gmail.com>
2022-06-10 20:03:35 +0200

    organized files

---
+++ js/form.js
@@ -0,0 +1,36 @@
+let botonAdicionar = document.querySelector('#adicionar-paciente');
+
+botonAdicionar.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  let form = document.querySelector('#form-adicionar');
+
+  let nombre = form.nombre.value;
+  let peso = form.peso.value;
+  let altura = form.altura.value;
+  let grasa = form.grasa.value;
+
+  let tabla = document.querySelector('#tabla-pacientes');
+
+  let pacienteTr = document.createElement('tr');
+  let nombreTd = document.createElement('td');
+  let pesoTd = document.createElement('td');
+  let alturaTd = document.createElement('td');
+  let grasaTd = document.createElement('td');
+  let imcTd = document.createElement('td');
+
+  nombreTd.textContent = nombre;
+  alturaTd.textContent = altura;
+  pesoTd.textContent = peso;
+  grasaTd.textContent = grasa;
+
+  pacienteTr.appendChild(nombreTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(grasaTd);
+  //pacienteTr.appendChild(nombreTd);
+
+  tabla.appendChild(pacienteTr);
+
+  console.log(pacienteTr);
+});
