e607899ec0c49714f230a1210814f919e8ef93f1
Irene Riportella <riportellairene@gmail.com>
2022-06-10 10:23:22 +0200

    added imc formula

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,13 @@
-let titulo = document.querySelector('.titulo');
-console.log(titulo.textContent);
-console.log(titulo);
-titulo.textContent = 'Buena Vida Nutrición';
-console.log('Estoy imprimiendo de una fuente externa que es principal.js');
+let paciente = document.querySelector('#primer-paciente');
+let tdpeso = paciente.querySelector('.info-peso');
+let peso = tdpeso.textContent;
+let tdaltura = paciente.querySelector('.info-altura');
+let altura = tdaltura.textContent;
+
+console.log(paciente);
+console.log(tdpeso);
+console.log(peso);
+console.log(altura);
+
+let imc = peso / (altura * altura);
+console.log(imc);
