e7ff1d3dbfc1ec7fe6c2ce6951ad85c9c6fd06d8
Irene Riportella <riportellairene@gmail.com>
2022-06-11 14:48:02 +0200

    created function exhibirMensajesErrores

--- js/form.js
+++ js/form.js
@@ -7,6 +7,14 @@ botonAdicionar.addEventListener('click', function (event) {
   let paciente = capturarDatosPaciente(form);
   let pacienteTr = construirTr(paciente);
 
+  let errores = validarPaciente(paciente);
+  console.log(errores);
+
+  if (errores.length > 0) {
+    exhibirMensajesErrores(errores);
+    return;
+  }
+
   let tabla = document.querySelector('#tabla-pacientes');
   tabla.appendChild(pacienteTr);
   form.reset();
@@ -46,4 +54,28 @@ function construirTd(dato, clase) {
   td.classList.add(clase);
   td.textContent = dato;
   return td;
+}
+
+function validarPaciente(paciente) {
+  let errores = [];
+  if (!validarPeso(paciente.peso)) {
+    errores.push = 'El peso es incorrecto';
+  } else {
+    return '';
+  }
+
+  if (!validarAltura(paciente.altura)) {
+    errores.push = 'La altura es incorrecta';
+  }
+  return errores;
+}
+
+function exhibirMensajesErrores(errores) {
+  let ul = document.querySelector('#mensajes-errores');
+
+  errores.forEach((error) => {
+    let li = document.createElement('li');
+    li.textContent = error;
+    ul.appendChild(li);
+  });
 }
