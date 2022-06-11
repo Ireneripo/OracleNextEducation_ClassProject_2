5ec27c7c1ea1c5a54771395e045fef8e28c9f654
Irene Riportella <riportellairene@gmail.com>
2022-06-11 10:52:12 +0200

    added classe to new patients

--- js/form.js
+++ js/form.js
@@ -4,19 +4,44 @@ botonAdicionar.addEventListener('click', function (event) {
   event.preventDefault();
 
   let form = document.querySelector('#form-adicionar');
-
   let paciente = capturarDatosPaciente(form);
-  console.log(paciente);
-
   let tabla = document.querySelector('#tabla-pacientes');
+  let pacienteTr = construirTr(paciente);
+  tabla.appendChild(pacienteTr);
+});
 
+//Capturar los datos del formulario
+function capturarDatosPaciente(form) {
+  let paciente = {
+    nombre: form.nombre.value,
+    peso: form.peso.value,
+    altura: form.altura.value,
+    grasa: form.grasa.value,
+    imc: calcularIMC(form.peso.value, form.altura.value),
+  };
+
+  return paciente;
+}
+
+function construirTr(paciente) {
   //Crear los tds y un tr
   let pacienteTr = document.createElement('tr');
+  pacienteTr.classList.add('paciente');
+
   let nombreTd = document.createElement('td');
+  nombreTd.classList.add('info-nombre');
+
   let pesoTd = document.createElement('td');
+  pesoTd.classList.add('info-peso');
+
   let alturaTd = document.createElement('td');
+  alturaTd.classList.add('info-altura');
+
   let grasaTd = document.createElement('td');
+  grasaTd.classList.add('info-grasa');
+
   let imcTd = document.createElement('td');
+  imcTd.classList.add('info-imc');
 
   //Asignar los valores a la propiedad textContent
   nombreTd.textContent = paciente.nombre;
@@ -32,18 +57,5 @@ botonAdicionar.addEventListener('click', function (event) {
   pacienteTr.appendChild(grasaTd);
   pacienteTr.appendChild(imcTd);
 
-  tabla.appendChild(pacienteTr);
-});
-
-//Capturar los datos del formulario
-function capturarDatosPaciente(form) {
-  let paciente = {
-    nombre: form.nombre.value,
-    peso: form.peso.value,
-    altura: form.altura.value,
-    grasa: form.grasa.value,
-    imc: calcularIMC(form.peso.value, form.altura.value),
-  };
-
-  return paciente;
+  return pacienteTr;
 }
