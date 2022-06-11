77394c09e3631f5d99a507e4320e1715320309d2
Irene Riportella <riportellairene@gmail.com>
2022-06-11 16:22:36 +0200

    improuved patient validation and function exhibirMensajesErrores

--- js/form.js
+++ js/form.js
@@ -8,7 +8,6 @@ botonAdicionar.addEventListener('click', function (event) {
   let pacienteTr = construirTr(paciente);
 
   let errores = validarPaciente(paciente);
-  console.log(errores);
 
   if (errores.length > 0) {
     exhibirMensajesErrores(errores);
@@ -18,9 +17,12 @@ botonAdicionar.addEventListener('click', function (event) {
   let tabla = document.querySelector('#tabla-pacientes');
   tabla.appendChild(pacienteTr);
   form.reset();
+
+  let mensajesErrores = document.querySelector('#mensajes-errores');
+  mensajesErrores.innerHTML = '';
 });
 
-//Capturar los datos del formulario
+// Capturar los datos del formulario
 function capturarDatosPaciente(form) {
   let paciente = {
     nombre: form.nombre.value,
@@ -33,12 +35,12 @@ function capturarDatosPaciente(form) {
   return paciente;
 }
 
-//Crear una fila de la tabla
+// Crear una fila de la tabla
 function construirTr(paciente) {
   let pacienteTr = document.createElement('tr');
   pacienteTr.classList.add('paciente');
 
-  //Agregar los tds al tr y el tr a la tabla
+  // Agregar los tds al tr y el tr a la tabla
   pacienteTr.appendChild(construirTd(paciente.nombre, 'info-nombre'));
   pacienteTr.appendChild(construirTd(paciente.peso, 'info-peso'));
   pacienteTr.appendChild(construirTd(paciente.altura, 'info-altura'));
@@ -48,7 +50,7 @@ function construirTr(paciente) {
   return pacienteTr;
 }
 
-//Crear un td
+// Crear un td
 function construirTd(dato, clase) {
   let td = document.createElement('td');
   td.classList.add(clase);
@@ -56,22 +58,40 @@ function construirTd(dato, clase) {
   return td;
 }
 
+// Validar un paciente
 function validarPaciente(paciente) {
   let errores = [];
+
+  if (paciente.nombre.length === 0) {
+    errores.push('El nombre no puede estar vacío');
+  }
+
+  if (paciente.peso.length === 0) {
+    errores.push('El peso no puede estar vacío');
+  }
+
+  if (paciente.altura.length === 0) {
+    errores.push('La altura no puede estar vacía');
+  }
+
+  if (paciente.grasa.length === 0) {
+    errores.push('El porcentaje de grasa no puede estar vacío');
+  }
+
   if (!validarPeso(paciente.peso)) {
-    errores.push = 'El peso es incorrecto';
-  } else {
-    return '';
+    errores.push('El peso es incorrescto');
   }
 
   if (!validarAltura(paciente.altura)) {
-    errores.push = 'La altura es incorrecta';
+    errores.push('La altura es incorrecta');
   }
   return errores;
 }
 
+// Mostrar mensaje de error
 function exhibirMensajesErrores(errores) {
   let ul = document.querySelector('#mensajes-errores');
+  ul.innerHTML = '';
 
   errores.forEach((error) => {
     let li = document.createElement('li');
