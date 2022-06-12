3aba831db9d8df845844fcbb9b4d8e34970c0b28
Irene Riportella <riportellairene@gmail.com>
2022-06-12 16:06:55 +0200

    added patient filter

---
+++ js/filtrar.js
@@ -0,0 +1,24 @@
+let campoFiltro = document.querySelector('#filtrar-tabla');
+
+campoFiltro.addEventListener('input', function () {
+  let pacientes = document.querySelectorAll('.paciente');
+
+  if (this.value.length > 0) {
+    for (let i = 0; i < pacientes.length; i++) {
+      let paciente = pacientes[i];
+      let tdNombre = paciente.querySelector('.info-nombre');
+      let nombre = tdNombre.textContent;
+
+      if (nombre !== this.value) {
+        paciente.classList.add('invisible');
+      } else {
+        paciente.classList.remove('invisible');
+      }
+    }
+  } else {
+    for (let i = 0; i < pacientes.length; i++) {
+      let paciente = pacientes[i];
+      paciente.classList.remove('invisible');
+    }
+  }
+});
